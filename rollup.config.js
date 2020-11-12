8e4bc4eca6b3c5098b2051b5d411abfcb6a17b28
Alexey Kruglov <leshak@leshak.ru>
2020-11-12 17:54:41 +0300

    init site

--- rollup.config.js
+++ rollup.config.js
@@ -33,7 +33,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		file: 'assets/bundle.js'
 	},
 	plugins: [
 		svelte({
@@ -63,7 +63,7 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload('./'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
