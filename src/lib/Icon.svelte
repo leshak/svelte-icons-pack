85fdc465f4dca5f81b233a659f87278471e3a179
LeshaK <leshak@leshak.ru>
2024-03-27 10:19:01 +0300

    add lib

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { IconType } from './index.js';
+
+	export let src: IconType;
+	export let size: string | number = '1em';
+	export let viewBox: string = undefined;
+	export let color: string = 'currentColor';
+	export let title: string = undefined;
+	export let className: string = '';
+
+	let innerHtml: string;
+	let attr: any = {};
+	$: {
+		attr = {};
+		if (color) {
+			if (src.a.stroke !== 'none') {
+				attr.stroke = 'currentColor';
+			}
+			if (src.a.fill !== 'none') {
+				attr.fill = 'currentColor';
+			}
+		}
+	}
+
+	$: {
+		innerHtml = (title ? `<title>${title}</title>` : '') + src.c;
+	}
+</script>
+
+<svg
+	width={size}
+	height={size}
+	{viewBox}
+	style={color ? 'color: ' + color + ';' : ''}
+	stroke-width="0"
+	class={className}
+	{...src.a}
+	{...attr}
+	xmlns="http://www.w3.org/2000/svg"
+>
+	{@html innerHtml}
+</svg>
---
+++ src/lib/index.ts
@@ -0,0 +1,8 @@
+import Icon from './Icon.svelte';
+
+export type IconType = {
+	a: object;
+	c: string;
+};
+
+export { Icon };
